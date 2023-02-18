1b8d9cfeba83f3ce9cb6b1498240262c2a3b7cd8
alincnl <alinashitenko2004@gmail.com>
2023-02-18 05:50:12 +0000

    add time

--- gpu1.cpp
+++ gpu1.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <chrono>
 #include <cmath>
 using namespace std;
 
 int n = 10000000;
-float temp = 2 * 3.141592 / n;
-float arr_sum = 0;
+double temp = 2 * 3.141592 / n;
+double arr_sum = 0;
 
-void fill_sin(float* a) {
+void fill_sin(double* a) {
     #pragma acc data create(a[:n]) copy(arr_sum) copyin(temp)
     {
     #pragma acc kernels
@@ -23,8 +24,12 @@ void fill_sin(float* a) {
     cout << arr_sum << endl;
 }
 int main() {
-    float* a = new float[n];
-    fill_sin(a);
-    delete[] a;
-    return 0;
+    auto begin = std::chrono::steady_clock::now();
+	double* a = new double[n];
+	fill_sin(a);
+	delete[] a;
+	auto end = std::chrono::steady_clock::now();
+	auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    std::cout << "The time: " << elapsed_ms.count() << " ms\n";
+	return 0;
 }
